342114f67f463a2d242b7e6a355e53be4469a996
Jesus A. Gonzales <jagjaek@gmail.com>
2024-10-20 03:27:57 -0500

    Creacion del Archivo Grupal

---
+++ Trabajo_final_grupo3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 03:21:17 2024
+
+@author: jagon
+"""
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import time
+import datetime
+import streamlit as st
+
+st.title('Mi Primer Proyecto')
+st.write('Hola **como** estas')
+appointment = st.slider(label = 'Programe Horario de asesoria',
+                        value= (time(11,30), time(12,45)))
